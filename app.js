df67dff6743a4e3fda5c25ae51b96ba0c3098c65
Bassole-9 <bassolearnaud9@gmail.com>
2023-10-26 09:21:40 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express"
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { config } from "dotenv"
+import { connecterDb } from "./db/db.js"
+import userRouter from "./routes/user.js"
+config()
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log(__dirname)
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")))
+
+// app.use("/appeluser/user",userRouter)
+// app.use("/appeluser/article",articleRouter)
+
+app.use("/api/users", userRouter)
+
+connecterDb().then(()=>{
+    app.listen(3000,()=>{
+        console.log("server en marche");
+    })
+
+}).catch((error)=>{
+console.log(error.message);
+})
